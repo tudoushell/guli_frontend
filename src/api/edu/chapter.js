4f514b0513abfc64513f810b4fd6065162808903
happy <whitehatzk520@gmail.com>
2020-07-14 22:48:09 +0800

    完成章节和小节功能

--- src/api/edu/chapter.js
+++ src/api/edu/chapter.js
@@ -1,6 +1,25 @@
 import request from "@/utils/request";
 
 export default {
+  updateChapter(chapterDto) {
+    return request({
+      url: `/edu-service/edu-chapter`,
+      method: "put",
+      data: chapterDto
+    });
+  },
+  getChapter(id) {
+    return request({
+      url: `/edu-service/edu-chapter/chapter/${id}`,
+      method: "get"
+    });
+  },
+  deleteChapter(id) {
+    return request({
+      url: `/edu-service/edu-chapter/${id}`,
+      method: "delete"
+    });
+  },
   addChapter(chapterDto) {
     return request({
       url: `/edu-service/edu-chapter`,
--- src/views/edu/course/chapter.vue
+++ src/views/edu/course/chapter.vue
@@ -17,8 +17,18 @@
               <el-button @click="showSectionDialog(chapter.id)" type="primary"
                 >添加小节</el-button
               >
-              <el-button type="primary" icon="el-icon-edit"></el-button>
-              <el-button type="primary" icon="el-icon-delete"></el-button>
+              <!-- 章节修改 -->
+              <el-button
+                type="primary"
+                @click="eidtorChapter(chapter.id)"
+                icon="el-icon-edit"
+              ></el-button>
+              <!-- 章节删除 -->
+              <el-button
+                type="primary"
+                @click="deleteChapter(chapter.id)"
+                icon="el-icon-delete"
+              ></el-button>
             </div>
           </div>
           <ul>
@@ -32,7 +42,11 @@
                 </div>
                 <div class="button-collect-style">
                   <!-- 编辑小节 -->
-                  <el-button type="primary" icon="el-icon-edit"></el-button>
+                  <el-button
+                    type="primary"
+                    @click="editorSectionChapter(chapter.id, sectionChapter.id)"
+                    icon="el-icon-edit"
+                  ></el-button>
                   <!-- 删除小节 -->
                   <el-button
                     type="primary"
@@ -46,7 +60,10 @@
         </li>
       </ul>
       <!-- 章节弹窗 -->
-      <el-dialog title="添加章节" :visible.sync="dialogFormVisible">
+      <el-dialog
+        :title="isShowAddOrUpdateChapter ? '添加章节' : '修改章节'"
+        :visible.sync="dialogFormVisible"
+      >
         <el-form :model="chapterDto">
           <el-form-item label="章节名称">
             <el-input v-model="chapterDto.title" autocomplete="off"></el-input>
@@ -62,11 +79,16 @@
         </el-form>
         <div slot="footer" class="dialog-footer">
           <el-button @click="dialogFormVisible = false">取 消</el-button>
-          <el-button type="primary" @click="saveChapter">确 定</el-button>
+          <el-button type="primary" @click="saveOrUpdateChapter"
+            >确 定</el-button
+          >
         </div>
       </el-dialog>
       <!-- 小节弹窗 -->
-      <el-dialog title="添加小节" :visible.sync="sectionDialog">
+      <el-dialog
+        :title="isShowAddOrUpdateSectionChapter ? '添加小节' : '修改小节'"
+        :visible.sync="sectionDialog"
+      >
         <el-form :model="sectionChapter">
           <el-form-item label="小节名称">
             <el-input
@@ -84,7 +106,7 @@
           </el-form-item>
         </el-form>
         <div slot="footer" class="dialog-footer">
-          <el-button @click="sectionDialog = false">取 消</el-button>
+          <el-button @click="closeSectionChapterDialog">取 消</el-button>
           <el-button type="primary" @click="saveOrUpdateSectionChapter"
             >确 定</el-button
           >
@@ -111,6 +133,7 @@ export default {
       dialogFormVisible: false,
       sectionDialog: false,
       chapterDto: {
+        id: "",
         courseId: "",
         title: "",
         sort: 1
@@ -118,13 +141,15 @@ export default {
       chapterDtoList: [],
       //小节
       sectionChapter: {
+        id: "",
         courseId: "",
         chapterId: "",
         title: "",
         sort: 1
       },
-      //章节ID
-      chapterId: ""
+      //用于显示添加或修改小节的名称
+      isShowAddOrUpdateSectionChapter: true,
+      isShowAddOrUpdateChapter: true
     };
   },
   created() {
@@ -134,9 +159,44 @@ export default {
     }
   },
   methods: {
+    //弹出章节信息
+    eidtorChapter(chapterId) {
+      this.chapterDto.id = chapterId;
+      this.isShowAddOrUpdateChapter = false;
+      this.dialogFormVisible = true;
+      chapterApi.getChapter(chapterId).then(response => {
+        this.chapterDto = response.data;
+      });
+    },
+    //删除章节
+    deleteChapter(chapterId) {
+      this.$confirm("此操作将删除章节, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      }).then(() => {
+        chapterApi.deleteChapter(chapterId).then(response => {
+          this.$message({
+            type: "success",
+            message: "删除成功!"
+          });
+          //刷新页面
+          this.listChapter();
+        });
+      });
+    },
+    //编辑小节信息
+    editorSectionChapter(chapterId, sectionChapterId) {
+      this.sectionChapter.id = sectionChapterId;
+      this.chapterDto.id = chapterId;
+      this.sectionDialog = true;
+      this.isShowAddOrUpdateSectionChapter = false;
+      videoApi.getVideo(sectionChapterId).then(response => {
+        this.sectionChapter = response.data;
+      });
+    },
     //删除小节
     deleteSectionChapter(sectionChapterId) {
-      console.log(sectionChapterId);
       this.$confirm("此操作将删除该小节, 是否继续?", "提示", {
         confirmButtonText: "确定",
         cancelButtonText: "取消",
@@ -155,21 +215,52 @@ export default {
     //保存或更新小节
     saveOrUpdateSectionChapter() {
       this.sectionChapter.courseId = this.courseId;
-      this.sectionChapter.chapterId = this.chapterId;
-      videoApi.addVideo(this.sectionChapter).then(response => {
-        this.$message({
-          type: "success",
-          message: "添加小节成功!"
+      this.sectionChapter.chapterId = this.chapterDto.id;
+      //添加小节信息
+      if (this.sectionChapter.id === "") {
+        videoApi.addVideo(this.sectionChapter).then(response => {
+          this.$message({
+            type: "success",
+            message: "添加小节成功!"
+          });
+          this.sectionDialog = false;
+          this.listChapter();
+          this.sectionChapter = {
+            id: "",
+            courseId: "",
+            chapterId: "",
+            title: "",
+            sort: 1
+          };
         });
-        this.sectionDialog = false;
-        this.listChapter();
-        this.sectionChapter = {};
-      });
+      } else {
+        //修改小节信息
+        videoApi.updateVideo(this.sectionChapter).then(response => {
+          this.$message({
+            type: "success",
+            message: "小节成功!"
+          });
+          this.sectionDialog = false;
+          this.listChapter();
+          this.sectionChapter = {
+            id: "",
+            courseId: "",
+            chapterId: "",
+            title: "",
+            sort: 1
+          };
+        });
+      }
+    },
+    //关闭小节dialog
+    closeSectionChapterDialog() {
+      this.sectionDialog = false;
+      this.sectionChapter = {};
     },
     //弹出小节dialog
     showSectionDialog(chapterId) {
       this.sectionDialog = true;
-      this.chapterId = chapterId;
+      this.chapterDto.id = chapterId;
     },
     //显示章节和小节
     listChapter() {
@@ -177,18 +268,33 @@ export default {
         this.chapterDtoList = response.data;
       });
     },
-    //添加章节
-    saveChapter() {
+    //添加或修改章节
+    saveOrUpdateChapter() {
+      this.isShowAddOrUpdateChapter = true;
       this.chapterDto.courseId = this.courseId;
-      chapterApi.addChapter(this.chapterDto).then(response => {
-        this.$message({
-          type: "success",
-          message: "添加章节成功!"
+      //添加章节
+      if (this.chapterDto.id === "") {
+        chapterApi.addChapter(this.chapterDto).then(response => {
+          this.$message({
+            type: "success",
+            message: "添加章节成功!"
+          });
+          this.dialogFormVisible = false;
+          this.listChapter();
+          this.chapterDto = { id: "", courseId: "", title: "", sort: 1 };
         });
-        this.dialogFormVisible = false;
-        this.listChapter();
-        this.chapterDto = "";
-      });
+      } else {
+        //修改章节
+        chapterApi.updateChapter(this.chapterDto).then(response => {
+          this.$message({
+            type: "success",
+            message: "修改章节成功!"
+          });
+          this.dialogFormVisible = false;
+          this.listChapter();
+          this.chapterDto = { id: "", courseId: "", title: "", sort: 1 };
+        });
+      }
     },
     showDialog() {
       this.dialogFormVisible = true;
